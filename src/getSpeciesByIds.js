d22fd88748d8957e3a6b9b4491f3b91f0fb45014
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-02-10 11:37:41 -0300

    adicionando correção pro lint requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -4,7 +4,7 @@ function getSpeciesByIds(...ids) {
   // buscar animal pela id?
   // console.log(ids);
   // //////////cada elemento do array///indo comparar//entrando no especies//pegando cada um // comparando
-  const idsParam = ids.map((itemArray) => data.species.find((elemento) => elemento.id === itemArray));
+  const idsParam = ids.map((item) => data.species.find((elemento) => elemento.id === item));
   /* const infoAnimals = data.species.filter((elemento) => elemento.id === ids); */
   return idsParam;
   // SE? não receber um parametro, retorne um array vazio vazio //FILTER FAZ ISSO
