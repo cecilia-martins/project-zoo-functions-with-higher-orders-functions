df7d756e1867a37bb0fd88b3ed23959fe425d2e1
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-02-10 17:16:55 -0300

    adicionando requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  // pegar animais
+  const tt = data.species.find((cadaEl) => cadaEl.name === animal);
+  // dever retornar um boleano
+  const tt2 = tt.residents.every((cadaRe) => cadaRe.age >= age);
+  return tt2;
 }
 
+// console.log(getAnimalsOlderThan('lion', 5));
 module.exports = getAnimalsOlderThan;
