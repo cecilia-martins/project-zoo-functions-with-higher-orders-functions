df6c49cec06d94ddd52d89135ced58429391934f
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-02-12 12:12:34 -0300

    adicionado requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,46 @@
-const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+// {
+//   "id": "4b40a139-d4dc-4f09-822d-ec25e819a5ad", // id da pessoa
+//   "fullName": "Sharonda Spry", // nome completo: firstName + lastName
+//   "species": [ "otters", "frogs" ], // espécies as quais a pessoa é responsável
+//   "locations": [ "SE", "SW" ], // Um array contendo todas as localizações das espécies
+// }
+function semParam() {
+  const tt = employees.map((cadaEmpregado) => {
+    const especies = species.filter((cadaAnimal) => cadaEmpregado.responsibleFor
+      .includes(cadaAnimal.id));
+    const nomeDoAnimal = especies.map((cadaAnimal) => cadaAnimal.name);
+    const localizaco = especies.map((cadaAnimal) => cadaAnimal.location);
+    const objeto = {
+      id: cadaEmpregado.id,
+      fullName: `${cadaEmpregado.firstName} ${cadaEmpregado.lastName}`,
+      species: nomeDoAnimal,
+      locations: localizaco,
+    };
+    return objeto;
+  });
+
+  return tt;
+}
+
+function procuraPorNomeESobrenome(objetoParam) {
+  const empreguetes = semParam();
+  const separando = empreguetes.find((cadaEmpreguete) => cadaEmpreguete
+    .fullName.includes(objetoParam.name) || cadaEmpreguete.id.includes(objetoParam.id));
+  return separando;
+}
+
+function getEmployeesCoverage(parametroSemGraca) {
+  if (parametroSemGraca === undefined) {
+    return semParam();
+  }
+  const ttt = employees.some((cadaEmp) => cadaEmp.id === parametroSemGraca.id
+  || cadaEmp.lastName === parametroSemGraca.name || cadaEmp.firstName === parametroSemGraca.name);
+  if (ttt) {
+    return procuraPorNomeESobrenome(parametroSemGraca);
+  }
+  throw new Error('Informações inválidas');
 }
 
 module.exports = getEmployeesCoverage;
