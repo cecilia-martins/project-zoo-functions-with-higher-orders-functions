eb6a2df8177b09e51832f0696fe96388c5aae83c
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-02-12 13:39:11 -0300

    adicionado requisito 09

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,20 @@
-const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+  const oFuncionario = employees.find((cadaEmpregado) => cadaEmpregado.id === id);
+  const primeiroIdAnimalResponsavel = oFuncionario.responsibleFor[0];
+  const primeiroAnimal = species
+    .find((cadaAnimal) => cadaAnimal.id === primeiroIdAnimalResponsavel);
+  const animalCerto = primeiroAnimal.residents
+    .reduce((accumulator, cadaResidente) => {
+      if (cadaResidente.age > accumulator.age) {
+        return cadaResidente;
+      }
+      return accumulator;
+    });
+  const retorno = Object.values(animalCerto);
 
+  return retorno;
+}
+console.log(getOldestFromFirstSpecies('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = getOldestFromFirstSpecies;
